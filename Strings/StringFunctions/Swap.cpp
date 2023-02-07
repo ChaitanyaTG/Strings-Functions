cd1b30843e9a6e32fa8dc507e190f6e3755383d7
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-02-07 06:18:00 +0530

    Add files via upload

---
+++ Strings/StringFunctions/Swap.cpp
@@ -0,0 +1,22 @@
+//
+//  Swap.cpp
+//  Strings
+//
+//  Created by Chaitanya Gadkari on 01/02/23.
+//
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1 = "Programming";
+    string str2 = "Language";
+    
+    str1.swap(str2);
+    cout<<str1<<endl<<str2<<endl;
+    
+    return 0;
+}
