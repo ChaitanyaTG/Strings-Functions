9adef1186bb2aa2940b4e907b93be3f49bd02cc6
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-02-09 17:17:56 +0530

    Add files via upload

---
+++ Strings/StringFunctions/Length&Size.cpp
@@ -0,0 +1,36 @@
+//
+//  Length&Size.cpp
+//  Strings
+//
+//  Created by Chaitanya Gadkari on 01/02/23.
+//
+
+
+//Length (str.length())
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str = "Hello";
+    cout<<str.length()<<endl;
+    
+    cout<<str<<endl;
+    
+    return 0;
+}
+
+// Size (str.size())
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str = "Welcome";
+    cout<<str.size()<<endl;
+    
+    cout<<str<<endl;
+    return 0;
+}
