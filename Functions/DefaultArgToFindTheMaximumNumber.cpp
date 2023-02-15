ead47de9bb6fd2c27d90d2f790cd8d4aa5b72d3b
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-02-15 22:50:59 +0530

    Add files via upload

---
+++ Functions/DefaultArgToFindTheMaximumNumber.cpp
@@ -0,0 +1,20 @@
+//
+//  DefaultArgToFindTheMaximumNumber.cpp
+//  Functions
+//
+//  Created by Chaitanya Gadkari on 10/02/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int max(int a = 0, int b = 0, int c = 0) {
+    return a>b && a>c ?a :b>c?b:c;
+}
+int main() {
+    cout<<max()<<endl;
+    cout<<max(10)<<endl;
+    cout<<max(10,13)<<endl;
+    cout<<max(10,13,15)<<endl;
+    return 0;
+}
